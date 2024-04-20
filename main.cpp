a68ff9c2d145d8692386b5f3d0e5d44d720a4dbf
tina-codes <155031784+quachtinaa@users.noreply.github.com>
2024-04-20 04:01:53 +0000

    updated choices GHI

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ using namespace std;
         Team home, visitors;
         string color, reset = "\x1b[0m";
       public:
-        Scoreboard(){minute = 0, seconds = 0, quarter = 0, down = 0, toGo = 0, home = Team(), visitors = Team();} // default constructor
+        Scoreboard(){minute = 0, seconds = 0, quarter = 1, down = 0, toGo = 0, home = Team(), visitors = Team();} // default constructor
         void setMinute(int min){minute = min;}
         void setSeconds(int sec){seconds = sec;}
         void setQuarter(int q){quarter = q;}
@@ -71,14 +71,16 @@ int main()
   Scoreboard s;
   Team tOne, tTwo;
   char choice = '\0';
+  int localMinutes = 0, localSeconds = 0, localDown = 0, localToGo = 0, localEtc = 0;
 
+  s.setHome(tOne);
+  s.setVisitors(tTwo);
+  
   // do while loop for menu
   do
     {
       s.showScoreboard();
-
-      cout << "-----------------------------------------------------------------------" << endl;
-      cout << "\n \t\t\t\t\t\t\t\t\tMENU" << endl;
+      cout << "\n\n \t\t\t\t\t\t\t\t\tMENU" << endl;
       cout << "-----------------------------------------------------------------------" << endl;
       cout << "Note: Team One is Home and Team Two is Visitor unless changed otherwise" << endl;
       cout << "-----------------------------------------------------------------------" << endl;
@@ -87,15 +89,61 @@ int main()
       cout << "C. Update Team Coach Names" << endl;
       cout << "D. Update Home City Names" << endl;
       cout << "E. Update Scores" << endl;
-      cout << "J. Update Timeout Counts" << endl;
-      cout << "F. Update Quarter" << endl;
-      cout << "G. Update Down" << endl;
-      cout << "H. Update To Go" << endl;
+      cout << "F. Update Timeout Counts" << endl;
+      cout << "G. Update Quarter" << endl;
+      cout << "H. Update Down and To Go Count" << endl;
       cout << "I. Update Time" << endl;
+      cout << "X. Exit" << endl;
       cout << "\nEnter your choice: ";
       cin >> choice;
-    
-    } while (choice == 'X' || choice == 'x');
+
+      if (choice == 'A' || choice == 'a')
+      {
+      }
+      if (choice == 'B' || choice == 'b')
+      {
+      }
+      if (choice == 'C' || choice == 'c')
+      {
+      }
+      if (choice == 'D' || choice == 'd')
+      {
+      }
+      if (choice == 'E' || choice == 'e')
+      {
+      }
+      if (choice == 'F' || choice == 'f')
+      {
+      }
+      if (choice == 'G' || choice == 'g')
+      {
+        cout << "\nQuarter: ";
+        cin >> localEtc;
+        s.setQuarter(localEtc);
+      }
+      if (choice == 'H' || choice == 'h')
+      {
+        cout << "\nDown: ";
+        cin >> localDown;
+        s.setDown(localDown);
+        cout << "\nTo Go: ";
+        cin >> localToGo;
+        s.setToGo(localToGo);
+      }
+      if (choice == 'I' || choice == 'i')
+      {
+        cout << "\nMinutes: ";
+        cin >> localMinutes;
+        s.setMinute(localMinutes);
+        cout << "\nSeconds: ";
+        cin >> localSeconds;
+        s.setSeconds(localSeconds);
+      }
+
+      s.setHome(tOne); // refresh the data in s for the new updates
+      s.setVisitors(tTwo);
+      cout << "\033[2J\033[1;1H"; // clear screen
+    } while (choice != 'X' || choice != 'x');
 
   return 0;
 }
